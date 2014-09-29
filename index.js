65c07c9f4b4f4ffd7944750509d6ce0546863a20
you21979 <you2197901@gmail.com>
2014-09-29 17:30:09 +0900

    add trading api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.CONSTANT = require('./lib/constant');
 exports.BasicApi = require('./lib/basic_api');
 exports.BasicApiUtil = require('./lib/basic_api_util');
+exports.TradingApi = require('./lib/trading_api');
