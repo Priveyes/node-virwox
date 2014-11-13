3cfbf2a2837dd856e86b1cc76dc63ce37a24f2bd
you21979 <you2197901@gmail.com>
2014-11-13 17:13:03 +0900

    mod

--- example/bitcoin_rate_fee.js
+++ example/bitcoin_rate_fee.js
@@ -8,9 +8,10 @@ Promise.all([
     util.getBitcoinRate()
 ]).spread(function(c,b){
     var feerate = 1.091;
-    b['BTC/JPY'] = b['BTC/USD'].map(function(v){return feerate * v * c['USDJPY']});
-    b['BTC/CNY'] = b['BTC/USD'].map(function(v){return feerate * v * c['USDCNY']});
-    var yahoo = {'BTC/USD':c['BTCUSD'],'BTC/JPY':c['BTCJPY'],'BTC/CNY':c['BTCCNY']};
+    b['BTC/USD'] = b['BTC/USD'].map(function(v){return v * feerate});
+    b['BTC/JPY'] = b['BTC/USD'].map(function(v){return v * c['USDJPY']});
+    b['BTC/CNY'] = b['BTC/USD'].map(function(v){return v * c['USDCNY']});
+    var yahoo = {'USD/JPY':c['USDJPY'],'BTC/USD':c['BTCUSD'],'BTC/JPY':c['BTCJPY'],'BTC/CNY':c['BTCCNY']};
     var vw = {'BTC/USD':b['BTC/USD'],'BTC/JPY':b['BTC/JPY'],'BTC/CNY':b['BTC/CNY']};
     return [yahoo,vw];
 }).then(console.log)
