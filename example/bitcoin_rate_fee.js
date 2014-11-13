86c9eede4f5c89624af7874c344c7920efcd6968
you21979 <you2197901@gmail.com>
2014-11-13 17:08:37 +0900

    add rate with fee

---
+++ example/bitcoin_rate_fee.js
@@ -0,0 +1,17 @@
+var Virwox = require('..');
+var util = Virwox.BasicApiUtil;
+var Promise = require('bluebird');
+var fx = require('yahoo-currency');
+
+Promise.all([
+    fx.fullRate(),
+    util.getBitcoinRate()
+]).spread(function(c,b){
+    var feerate = 1.091;
+    b['BTC/JPY'] = b['BTC/USD'].map(function(v){return feerate * v * c['USDJPY']});
+    b['BTC/CNY'] = b['BTC/USD'].map(function(v){return feerate * v * c['USDCNY']});
+    var yahoo = {'BTC/USD':c['BTCUSD'],'BTC/JPY':c['BTCJPY'],'BTC/CNY':c['BTCCNY']};
+    var vw = {'BTC/USD':b['BTC/USD'],'BTC/JPY':b['BTC/JPY'],'BTC/CNY':b['BTC/CNY']};
+    return [yahoo,vw];
+}).then(console.log)
+
